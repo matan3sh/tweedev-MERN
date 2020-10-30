60352f3b3856764e4cd3cc0418d5ae60d1c2a70d
Matan Shaviro <matan3sh@gmail.com>
2020-10-30 16:57:26 +0200

    Create add and update profile endpoint, get all profiles

--- backend/api/profile/profile.routes.js
+++ backend/api/profile/profile.routes.js
@@ -1,12 +1,43 @@
 const express = require('express');
-const { getProfile } = require('./profile.controller');
+const { check } = require('express-validator');
+const {
+  getProfiles,
+  getProfile,
+  submitUserProfile,
+  getProfileByUserId,
+} = require('./profile.controller');
 const auth = require('../../middleware/auth');
 
 const router = express.Router();
+
+// @desc Get all users profiles
+// @route GET /api/profile
+// @access Public
+router.get('/', getProfiles);
 
 // @desc Get current user profile
 // @route GET /api/profile/me
 // @access Private
 router.get('/me', auth, getProfile);
+
+// @desc Create or Update user profile
+// @route POST /api/profile
+// @access Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'Status is required').not().isEmpty(),
+      check('skills', 'Skills is required').not().isEmpty(),
+    ],
+  ],
+  submitUserProfile
+);
+
+// @desc Get profile by user Id
+// @route GET /api/profile/user/:userId
+// @access Public
+router.get('/user/:userId', getProfileByUserId);
 
 module.exports = router;
