3f216e7847f9563c0a3b2e7d442ddb52d0a712c8
Matan Shaviro <matan3sh@gmail.com>
2020-11-04 17:03:10 +0200

    Deploy to Heroku

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const connectDB = require('./config/db');
 
 const app = express();
@@ -16,6 +17,18 @@ const profileRoutes = require('./api/profile/profile.routes');
 app.use('/api/profile', profileRoutes);
 const postRoutes = require('./api/post/post.routes');
 app.use('/api/posts', postRoutes);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is runing...');
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server Started on Port ${PORT}`));
